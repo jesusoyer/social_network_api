fc7404b11373cdc1cbe24b8fb9946c1bf4015574
jesusoyer <oyervidesjesus017@gmail.com>
2022-07-25 13:55:21 -0500

    created routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes')
+const appRoutes = require('./appRoutes');
 
+
+router.use('/apps', appRoutes);
 router.use('/users', userRoutes)
 
 
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
+router.use((req, res)=> {
+    return res.send('wrong route!')
+});
 
 
 module.exports = router;
